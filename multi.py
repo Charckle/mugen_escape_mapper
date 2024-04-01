ed0b7e3b4854d777b2af957f9cd422842342775a
charckle <tsobw@email.com>
2024-04-01 19:41:06 +0200

    added fancy art

--- multi.py
+++ multi.py
@@ -2,12 +2,23 @@ import pygame
 from pygame.locals import *
 import pyautogui
 
+print("----------------------------------+")
+print(r"""___________                                                 _____                              
+\_   _____/ ______ ____           _____  ______   ____     /     \  __ __  ____   ____   ____  
+ |    __)_ /  ___// ___\   ______ \__  \ \____ \_/ __ \   /  \ /  \|  |  \/ ___\_/ __ \ /    \ 
+ |        \\___ \\  \___  /_____/  / __ \|  |_> >  ___/  /    Y    \  |  / /_/  >  ___/|   |  \
+/_______  /____  >\___  >         (____  /   __/ \___  > \____|__  /____/\___  / \___  >___|  /
+        \/     \/     \/               \/|__|        \/          \/     /_____/      \/     \/ 
+""")
+print("---------------+")
+print("v0.0.1")
+
 # Initialize pygame
 pygame.init()
 
 # Get the number of joysticks connected
 num_joysticks = pygame.joystick.get_count()
-print(num_joysticks)
+print(f"Number of controllers connected: {num_joysticks}")
 
 # Initialize all connected controllers
 controllers = [pygame.joystick.Joystick(i) for i in range(num_joysticks)]
